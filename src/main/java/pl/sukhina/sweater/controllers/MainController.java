8aaedac22d6b78770c22010408fba8acb8cdc353
Volodymyr <sukhina.vova@gmail.com>
2021-08-26 17:30:22 +0300

    Added possibility to upload and download images, created style.css

--- src/main/java/pl/sukhina/sweater/controllers/MainController.java
+++ src/main/java/pl/sukhina/sweater/controllers/MainController.java
@@ -1,21 +1,30 @@
 package pl.sukhina.sweater.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import pl.sukhina.sweater.models.Message;
 import pl.sukhina.sweater.models.User;
 import pl.sukhina.sweater.services.message.MessageService;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Controller
 public class MainController {
 
     final MessageService messageService;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping
     public String greeting  (Model model) {
@@ -38,8 +47,18 @@ public class MainController {
     @PostMapping("/main")
     public String sendMessage(
             @AuthenticationPrincipal User user, @RequestParam String text,
-            @RequestParam String tag, Model model) {
+            @RequestParam String tag, Model model, @RequestParam("file") MultipartFile file) throws IOException {
         Message message = new Message(text, tag, user);
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+            message.setFilename(resultFilename);
+        }
         messageService.createMessage(message);
         var messages = messageService.getMessages();
         model.addAttribute("messages", messages);
